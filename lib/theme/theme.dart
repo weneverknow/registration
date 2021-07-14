fd1ffc49d2b28a63352b2669fe32ecff36dfeb0a
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 01:24:40 +0700

    create color and padding value for theming apps

---
+++ lib/theme/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff3185F0);
+const greyColor = Color(0xffDFDFDF);
+const secondaryColor = Color(0xff76ACF1);
+
+const defaultPadding = 24.0;
