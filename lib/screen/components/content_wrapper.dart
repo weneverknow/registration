1b598dfdacd91e542f37b5f917b4a7ea84d417c8
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 21:57:49 +0700

    remove height property of container. add Alignment as parameter

--- lib/screen/components/content_wrapper.dart
+++ lib/screen/components/content_wrapper.dart
@@ -1,22 +1,22 @@
 part of 'components.dart';
 
 class ContentWrapper extends StatelessWidget {
-  const ContentWrapper({required this.child, Key? key}) : super(key: key);
+  const ContentWrapper({required this.child, this.alignment, Key? key})
+      : super(key: key);
   final Widget child;
+  final Alignment? alignment;
 
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.center,
-      child: FittedBox(
-        child: Container(
+      alignment: alignment ?? Alignment.center,
+      child: Container(
 //          color: Colors.red,
-          width: ScreenSize.screenWidth(context),
-          //height: ScreenSize.screenHeight(context) * 0.75,
-          padding: EdgeInsets.symmetric(
-              horizontal: defaultPadding, vertical: defaultPadding / 2),
-          child: child,
-        ),
+        width: ScreenSize.screenWidth(context),
+        //height: ScreenSize.screenHeight(context) * 0.75,
+        padding: EdgeInsets.symmetric(
+            horizontal: defaultPadding, vertical: defaultPadding / 2),
+        child: child,
       ),
     );
   }
