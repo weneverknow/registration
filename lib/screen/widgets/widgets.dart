f4b13391c4f1be4669343af964d95b81199a0972
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 16:10:58 +0700

    add new file to part

--- lib/screen/widgets/widgets.dart
+++ lib/screen/widgets/widgets.dart
@@ -4,3 +4,8 @@ import 'package:registration/theme/theme.dart';
 
 part 'circle_number.dart';
 part 'custom_button.dart';
+part 'title_text.dart';
+part 'subtitle_text.dart';
+part 'default_input_field.dart';
+part 'caption_text.dart';
+part 'error_text.dart';
