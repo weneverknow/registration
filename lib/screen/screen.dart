5cfd0a7576befa525c027025da24d1389ccb68bc
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 16:08:41 +0700

    change from scaffold to just for wrap FirstPage

--- lib/screen/screen.dart
+++ lib/screen/screen.dart
@@ -1,48 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:registration/screen/components/components.dart';
 import 'package:registration/screen/pages/pages.dart';
-import 'package:registration/screen/widgets/widgets.dart';
-import 'package:registration/theme/theme.dart';
 
-class Screen extends StatefulWidget {
+class Screen extends StatelessWidget {
   const Screen({Key? key}) : super(key: key);
 
   @override
-  _ScreenState createState() => _ScreenState();
-}
-
-class _ScreenState extends State<Screen> {
-  PageController _pageController = PageController();
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: greyColor,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              IndieBackground(),
-              PageView(
-                controller: _pageController,
-                children: [FirstPage(), SecondPage()],
-              ),
-              HeaderContainer(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate(
-                      4, (index) => CircleNumber(text: '${index + 1}')),
-                ),
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: CustomButton(
-          text: 'NEXT',
-        ));
+    return FirstPage();
   }
 }
