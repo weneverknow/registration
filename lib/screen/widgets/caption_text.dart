266e4a4a06b678b4bbbe400cc32162ae7cfb0500
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 21:59:05 +0700

    add Color as parameter

--- lib/screen/widgets/caption_text.dart
+++ lib/screen/widgets/caption_text.dart
@@ -1,14 +1,19 @@
 part of 'widgets.dart';
 
 class CaptionText extends StatelessWidget {
-  const CaptionText({required this.text, Key? key}) : super(key: key);
+  const CaptionText({required this.text, this.color, Key? key})
+      : super(key: key);
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: Theme.of(context).textTheme.caption,
+      style: Theme.of(context)
+          .textTheme
+          .caption!
+          .copyWith(color: color ?? Theme.of(context).textTheme.caption!.color),
     );
   }
 }
--- lib/screen/widgets/subtitle_text.dart
+++ lib/screen/widgets/subtitle_text.dart
@@ -1,14 +1,17 @@
 part of 'widgets.dart';
 
 class SubTitleText extends StatelessWidget {
-  const SubTitleText({required this.text, Key? key}) : super(key: key);
+  const SubTitleText({required this.text, this.color, Key? key})
+      : super(key: key);
   final String text;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: Theme.of(context).textTheme.subtitle1,
+      style: Theme.of(context).textTheme.subtitle1!.copyWith(
+          color: color ?? Theme.of(context).textTheme.subtitle1!.color),
     );
   }
 }
