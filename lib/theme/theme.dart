036bb1e4576eb26c6716a05a115ff9fd8d7accfa
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 09:18:30 +0700

    create TextStyle variable

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,3 +5,9 @@ const greyColor = Color(0xffDFDFDF);
 const secondaryColor = Color(0xff76ACF1);
 
 const defaultPadding = 24.0;
+
+TextStyle bigTitleText = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w700,
+  color: Colors.black,
+);
