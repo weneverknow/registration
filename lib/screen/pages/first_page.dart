e848d16701ca36959ab1bd0b3ed2ad0084f52bc7
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 16:09:20 +0700

    change to be scaffold

--- lib/screen/pages/first_page.dart
+++ lib/screen/pages/first_page.dart
@@ -1,75 +1,125 @@
 part of 'pages.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
 
   @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  late TextEditingController _emailController;
+  bool isValid = false;
+  bool _showError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      print('document ready');
+    });
+    isValid = false;
+    _showError = false;
+    _emailController = TextEditingController(text: '');
+  }
+
+  Future<void> onClick(bool isError) async {
+    await Future.delayed(Duration(milliseconds: 400));
+    setState(() {
+      _showError = isError;
+      _emailController.text = '';
+      isValid = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //final size = MediaQuery.of(context).size;
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: ClipPath(
-        clipper: MyClipper(),
-        child: SingleChildScrollView(
-          child: Container(
-            width: ScreenSize.screenWidth(context),
-            height: (ScreenSize.screenHeight(context) * 0.8) - 62,
-            color: greyColor,
-            padding: EdgeInsets.fromLTRB(
-                defaultPadding, (defaultPadding * 3), defaultPadding, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                TitleFirstPage(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-                  child: Text(
-                    'Welcome to The Bank of The Future.\nManage and track your accounts on the go.',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6!
-                        .copyWith(color: Colors.black),
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-                const SizedBox(
-                  height: defaultPadding,
-                ),
-                Container(
-                  height: 80,
-                  padding: EdgeInsets.symmetric(
-                      vertical: defaultPadding / 2,
-                      horizontal: defaultPadding / 2),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12)),
-                  alignment: Alignment.center,
-                  child: Container(
-                    height: 70,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: greyColor),
-                    alignment: Alignment.center,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Email',
-                          prefixIcon: Icon(Icons.mail_outline_outlined),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none),
+    return Scaffold(
+      backgroundColor: greyColor,
+      body: Stack(
+        children: [
+          IndieBackground(),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: ClipperBackground(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    TitleFirstPage(),
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(vertical: defaultPadding),
+                      child: Text(
+                        'Welcome to The Bank of The Future.\nManage and track your accounts on the go.',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(color: Colors.black),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      height: defaultPadding,
+                    ),
+                    Container(
+                        height: 80,
+                        padding: EdgeInsets.symmetric(
+                            vertical: defaultPadding / 2,
+                            horizontal: defaultPadding / 2),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)),
+                        alignment: Alignment.center,
+                        child: DefaultInputField(
+                          prefixIcon: Icon(Icons.mail_outline_outlined),
+                          hintText: 'Email',
+                          controller: _emailController,
+                          onChanged: (val) {
+                            setState(() {
+                              isValid = val.length > 0;
+                            });
+                          },
+                        )),
+                    _showError
+                        ? Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: defaultPadding / 2,
+                                horizontal: defaultPadding / 4),
+                            child: Text(
+                              'email is not valid',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(color: Colors.red[300]),
+                            ),
+                          )
+                        : const SizedBox.shrink()
+                  ],
                 ),
-                // Flexible(
-                //     child: Align(
-                //   alignment: Alignment.bottomCenter,
-                //   child: CustomButton(text: 'NEXT'),
-                // ))
-              ],
-            ),
+              )),
+          HeaderContainer(
+            children: [
+              CircleNumber(text: '1'),
+              CircleNumber(text: '2'),
+              CircleNumber(text: '3'),
+              CircleNumber(text: '4')
+            ],
           ),
-        ),
+        ],
+      ),
+      bottomNavigationBar: CustomButton(
+        text: 'NEXT',
+        onPress: () async {
+          if (isValid) {
+            await onClick(false);
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => SecondPage()));
+          } else {
+            await onClick(true);
+          }
+        },
       ),
     );
   }
