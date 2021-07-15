512ca69dae1d9179bc26f17fc64d953d5453e770
Afrizalbotong <cak.bhot@gmail.com>
2021-07-15 16:06:06 +0700

    add part variable

--- lib/screen/components/components.dart
+++ lib/screen/components/components.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:registration/screen/screen_size.dart';
 import '../../theme/theme.dart';
+import '../widgets/widgets.dart';
 
 part 'title_first_page.dart';
 part 'indie_background.dart';
 part 'header_container.dart';
 part 'clipper_background.dart';
+part 'complexity_card.dart';
+part 'content_wrapper.dart';
+part 'back_button_header.dart';
