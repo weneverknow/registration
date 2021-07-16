da5a75ee361f1c0c848e907c256a2c1fffbdd9d4
Afrizalbotong <cak.bhot@gmail.com>
2021-07-16 23:24:10 +0700

    add fourth_page.dart into part

--- lib/screen/pages/pages.dart
+++ lib/screen/pages/pages.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:registration/screen/components/components.dart';
 import 'package:registration/screen/screen_size.dart';
 import 'package:registration/theme/theme.dart';
 import 'package:registration/screen/widgets/widgets.dart';
+import '../controller/calendar_animation.dart';
+import '../../extension/date_time_extension.dart';
+import '../../extension/time_extension.dart';
+import 'package:flutter/cupertino.dart';
 
 part 'first_page.dart';
 part 'second_page.dart';
